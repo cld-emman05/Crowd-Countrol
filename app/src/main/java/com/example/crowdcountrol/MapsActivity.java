1c551b5cd409619b4fe7ffac608886752aba72d0
Emmanuel <frances_callado@dlsu.edu.ph>
2019-09-25 16:26:42 +0800

    Phase 2.2
    Added Current Location of the user

--- app/src/main/java/com/example/crowdcountrol/MapsActivity.java
+++ app/src/main/java/com/example/crowdcountrol/MapsActivity.java
@@ -1,8 +1,20 @@
 package com.example.crowdcountrol;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
 import android.os.Bundle;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -10,9 +22,15 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
+        com.google.android.gms.location.LocationListener {
 
     private GoogleMap mMap;
+
+    GoogleApiClient client;
+    Location lastLoc;
+    LocationRequest reqLoc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,9 +56,68 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        /* Add a marker in Sydney and move the camera
+        LatLng manila = new LatLng(14.599313, 120.981833);
+        mMap.addMarker(new MarkerOptions().position(manila).title("Target marked: Manila"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(manila));
+
+         */
+
+        buildGoogleApiClient();
+        mMap.setMyLocationEnabled(true);
+    }
+
+    protected synchronized void buildGoogleApiClient(){
+        client = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API).build();
+
+        client.connect();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        lastLoc = location;
+
+        LatLng latlng = new LatLng(location.getLatitude(), location.getLongitude());
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latlng));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+    }
+
+    /* @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    } */
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        reqLoc = new LocationRequest();
+        reqLoc.setInterval(1000);
+        reqLoc.setFastestInterval(1000);
+        reqLoc.setPriority(reqLoc.PRIORITY_HIGH_ACCURACY);
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(client, reqLoc, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
